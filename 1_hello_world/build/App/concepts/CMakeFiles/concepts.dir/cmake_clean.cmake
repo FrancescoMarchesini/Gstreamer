95004288b883e50e1d40d6bb7ba7b5d282ee39e8
FrancescoMarchesini <marchesinifrancesco1@gmail.com>
2018-04-04 13:53:53 +0200

    aggiunto cartelle tutorial

---
+++ 1_hello_world/build/App/concepts/CMakeFiles/concepts.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/concepts.dir/concepts_generated_cudaYUV-YV12.cu.o"
+  "CMakeFiles/concepts.dir/concepts_generated_cudaRGB.cu.o"
+  "CMakeFiles/concepts.dir/concepts_generated_cudaYUV-YUYV.cu.o"
+  "CMakeFiles/concepts.dir/concepts_generated_cudaYUV-NV12.cu.o"
+  "CMakeFiles/concepts.dir/main.cpp.o"
+  "CMakeFiles/concepts.dir/gstCamera.cpp.o"
+  "CMakeFiles/concepts.dir/gstUtility.cpp.o"
+  "CMakeFiles/concepts.dir/mygst.cpp.o"
+  "../../bin/aarch64/linux/release/concepts.pdb"
+  "../../bin/aarch64/linux/release/concepts"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/concepts.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
